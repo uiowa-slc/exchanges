8602bf04271b4bfa9ce33009794898196438eda7
Dustin Quam <quamsta@gmail.com>
2014-03-24 10:23:14 -0500

    converted to a vagrant/ss_env project updated ss

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,23 +3,22 @@
 global $project;
 $project = 'mysite';
 
-$path = realpath(dirname(__FILE__));
-$path = str_replace('mysite', '', $path);
-global $databaseConfig;
-
+global $database;
+$database = 'exchanges';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
 MySQLDatabase::set_connection_charset('utf8');
-
 // Set the current theme. More themes can be downloaded from
 // http://www.silverstripe.org/themes/
 SSViewer::set_theme('simple');
-
 // Set the site locale
 i18n::set_locale('en_US');
 
-error_reporting(E_ALL);
-
-Object::add_extension("GridFieldDetailForm_ItemRequest","GridFieldPublish");
-
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
-Object::add_extension('SiteConfig', 'CustomSiteConfig');
+/* Paste as Plain Text By Default */
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky',true);
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky_default',true);
+HtmlEditorConfig::get('cms')->insertButtonsBefore(
+    'styleselect',
+    'removeformat'
+);
