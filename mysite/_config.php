ec4a48e0ac4ba4404aa8f168088e854dcd0b23af
Dustin Quam <quamsta@gmail.com>
2014-03-25 12:38:02 -0500

    updated a few things before deployment

--- mysite/_config.php
+++ mysite/_config.php
@@ -8,20 +8,9 @@ $database = 'exchanges';
  
 // Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
-/*
-### @@@@ UPGRADE REQUIRED @@@@ ###
-FIND: MySQLDatabase::set_connection_charset(
-NOTE: Use "MySQLDatabase.connection_charset" config setting instead 
-### @@@@ ########### @@@@ ###
-*/MySQLDatabase::set_connection_charset('utf8');
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-/*
-### @@@@ UPGRADE REQUIRED @@@@ ###
-FIND: SSViewer::set_theme(
-NOTE: Deprecated, use config api 
-### @@@@ ########### @@@@ ###
-*/SSViewer::set_theme('simple');
+
+MySQLDatabase::set_connection_charset('utf8');
+
 // Set the site locale
 i18n::set_locale('en_US');
 
