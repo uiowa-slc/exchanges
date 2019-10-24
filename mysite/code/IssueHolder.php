dc6635f64a24f02f83bc7e6f7ccd2ae8361712ba
Brad Roggow <broggow@uiowa.edu>
2019-10-24 13:06:40 -0500

    Fixing minor formatting

--- mysite/code/IssueHolder.php
+++ mysite/code/IssueHolder.php
@@ -22,12 +22,7 @@ class IssueHolder extends Page {
 
 	public static function FeaturedIssue(){
 		//return $this->Children()->filter(['ParentID' => $this->ID])->sort('Created DESC')->First();
-
         $parentID = IssueHolder::get()->filter(['ClassName' => 'IssueHolder'])->sort('Created DESC')->First()->ID;
-        $parentIDs = array();
-        //foreach( $parents as $parent ) {
-        //    array_push( $parentIDs, $parent->ID );
-        //}
         return Issue::get()->filter(['ParentID' => $parentID])->sort('Created DESC')->First();
 	}
 }
