feae734e37b0bb19286d6bd80785ae5f8823c36d
Dustin Quam <quamsta@gmail.com>
2021-02-12 14:51:51 -0600

    file ownership

--- mysite/code/BlogPostExtension.php
+++ mysite/code/BlogPostExtension.php
@@ -14,6 +14,7 @@ class BlogPostExtension extends DataExtension {
 
 	private static $owns = array(
 		'AudioClip',
+		'AudioClipTranscript',
 	);
 
 	public function updateCMSFields(FieldList $fields) {
